7457cd4570d44cd08deec09c4bf1464262a5943a
shahroozk <shahrooz.kanani@gmail.com>
2019-02-12 19:02:15 -0800

    Updates

---
+++ precache-manifest.9572c89b16537aaf9806f5bd59be14d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e410ef131087a5d5cfb",
+    "url": "/React-Clicky-Game/static/js/runtime~main.4e410ef1.js"
+  },
+  {
+    "revision": "eea8dbb652427852276e",
+    "url": "/React-Clicky-Game/static/js/main.eea8dbb6.chunk.js"
+  },
+  {
+    "revision": "ba2021e67c053dfd7e1d",
+    "url": "/React-Clicky-Game/static/js/2.ba2021e6.chunk.js"
+  },
+  {
+    "revision": "eea8dbb652427852276e",
+    "url": "/React-Clicky-Game/static/css/main.ddeb9864.chunk.css"
+  },
+  {
+    "revision": "7fb05c2f1033f97c5cf2b91cbd8b5b6e",
+    "url": "/React-Clicky-Game/index.html"
+  }
+];
